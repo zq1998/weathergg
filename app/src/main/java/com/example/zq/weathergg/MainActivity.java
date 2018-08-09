a41253ac0d7615c50023231a9e09479b3cdb0350
zzz <1012872209@qq.com>
2018-08-09 10:41:59 +0800

    final

--- app/src/main/java/com/example/zq/weathergg/MainActivity.java
+++ app/src/main/java/com/example/zq/weathergg/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.zq.weathergg;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
